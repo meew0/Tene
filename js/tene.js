aea54d11d871c5cdf5269779ca4aa41c62bcfe9c
meew0 <blactbt@live.de>
2013-08-29 21:16:52 +0200

    stuff

--- js/tene.js
+++ js/tene.js
@@ -7,7 +7,6 @@ $('.d-input').each(function() {
 	$(this).addClass('i-unfocused-text');
 	$(this).val($(this).attr('tene-example'));
 });
-
 $('.d-input').focus(function() { 
 	if($(this).val() === $(this).attr('tene-example')) {
 		$(this).removeClass('i-unfocused-text');
@@ -20,8 +19,31 @@ $('.d-input').blur(function() {
 		
 		$(this).addClass('i-unfocused-text');
 		$(this).val($(this).attr('tene-example'));
-		console.log("Test");
 	}
+});
+
+$('.toggler').click(function() {
+	var toggled = $(this).attr('tene-toggle');
+	console.log(toggled);
+	switch($(this).attr('tene-effect')) {
+	case "slide":
+		$(toggled).slideToggle();
+		break;
+	case "fade":
+		$(toggled).fadeToggle();
+		break;
+	default:
+		$(toggled).toggle();
+		break;
+	}
+});
+
+$('.embed-yt').each(function() {
+	var dims = $(this).attr('tene-dims').split('x');
+	var w = dims[0];
+	var h = dims[1];
+	var vid = $(this).attr('tene-embed');
+	$(this).html('<iframe width="' + w + '" height="' + h + '" src="http://www.youtube.com/embed/' + vid + '" frameborder="0" allowfullscreen></iframe>');
 });
 
 });
